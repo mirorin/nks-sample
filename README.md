74cb4c21741e100b355b90b350a61b17113bf3a3
star76 <star76popin@gmail.com>
2020-01-05 03:46:46 +0900

    hoge

--- README.md
+++ README.md
@@ -1,20 +1,2 @@
-# nks-sample
-
-## Chart? 作成中
-
-先日のnginx-deploymentをそのままhelmの形に落とし込みたい  
-枠については  
-```
-$ helm create nginx-sample
-```
-で作っただけ、そこに先日のdelpoyment.yamlとService.yamlに相当するものをrenameの上移動  
-chartとしての体裁(deploy時に--setほにゃららでValueを変更できる形)には一切していない  
-そもそもこの形にするだけで動くのか不明なので試す必要がある
-
-### how to deploy LocalChart
-$ helm install --name nginx-sample path/to/chart/nginx-sample
-
-### dry run  
-$ helm install --name nginx-sample path/to/chart/nginx-sample --dry-run --debug
-
-
+# sample-admission-webhook
+Sample implementation of Kubernetes Admission Webhooks in Flask
---
+++ istio-vsvc-host/main.py
@@ -0,0 +1,73 @@
+import fnmatch
+
+from flask import Flask, jsonify, request
+from kubernetes import client, config
+
+
+app = Flask(__name__)
+
+config.load_incluster_config()
+
+# telepresenceではこっちを使う
+# config.load_kube_config()
+conf=client.Configuration()
+api_client=client.ApiClient(configuration=conf)
+
+@app.route('/validate', methods=['POST'])
+def validate():
+    try:
+        # リクエストから必要な情報を抜き出す
+        req = request.get_json()
+        new_hosts = req['request']['object']['spec']['hosts']
+        apiserver_resp = api_client.call_api(
+            '/apis/networking.istio.io/v1alpha3/namespaces/default/virtualservices',
+            'GET',
+            auth_settings=['BearerToken'],
+            response_type='object',
+        )
+        nested_existing_hosts = {
+            tuple(item['spec']['hosts']) for item in apiserver_resp[0]['items']
+        }
+        existing_hosts = {item for sublist in nested_existing_hosts for item in sublist}
+
+        print(f'existing_hosts: {existing_hosts}')
+        print(f'new_hosts: {new_hosts}')
+
+        # UPDATEのときは、oldに入っているものは検査対象から除外する
+        operation = req['request']['operation']
+        if operation == 'UPDATE':
+            old_hosts = req['request']['oldObject']['spec']['hosts']
+            for host in old_hosts:
+                existing_hosts.remove(host)
+            print(f'updated existing_hosts: {existing_hosts}')
+
+        # hostsの被りがないかチェックする
+        pair = get_collision_pair(new_hosts, existing_hosts)
+
+        if pair:
+            allowed = False
+            message = f'{pair[0]} collides with {pair[1]} which already exists'
+        else:
+            allowed = True
+            message = f'No collision detected'
+
+
+        # 結果を返す
+        return jsonify({
+            'apiVersion': 'admission.k8s.io/v1',
+            'kind': 'AdmissionReview',
+            'response': {
+                'uid': request.get_json()['request']['uid'],
+                'allowed': allowed,
+                'status': {'message': message}
+            }
+        }), 200
+
+    except (TypeError, KeyError):
+        return jsonify({'message': 'Invalid request'}), 400
+
+def get_collision_pair(new_hosts, existing_hosts):
+    for new_host in new_hosts:
+        for existing_host in existing_hosts:
+            if fnmatch.fnmatch(new_host, existing_host) or fnmatch.fnmatch(existing_host, new_host):
+                return new_host, existing_host
